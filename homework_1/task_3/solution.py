f529956efacd4b80087045133eb02cc95d7b87ee
Alexander <adbanned987@gmail.com>
2026-09-14 15:58:03 +0300

    Update solution.py
    Решение через Решето, а не n(sqrt(n))

--- homework_1/task_3/solution.py
+++ homework_1/task_3/solution.py
@@ -1,1 +1,18 @@
+def count_primes(N):
+    if N <= 2:
+        return 0
 
+    is_prime = [True] * N
+    is_prime[0] = False
+    is_prime[1] = False
+
+    p = 2
+
+    while p * p < N:
+        if is_prime[p]:
+            for multiple in range(p * p, N, p):
+                is_prime[multiple] = False
+
+        p += 1
+
+    return sum(is_prime)
